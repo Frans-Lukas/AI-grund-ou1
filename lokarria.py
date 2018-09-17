7c53d47481ad76fd9e44f5c791a776a4b75970bf
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-09-17 11:32:26 +0200

    started working on lookahead

--- lokarria.py
+++ lokarria.py
@@ -3,6 +3,9 @@
 import myJsonParser as myJsonParser
 import math
 from RestLokarria import *
+from Point import *
+import numpy as np
+
 MRDS_URL = 'localhost:50000'
 
 HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
@@ -11,55 +14,114 @@ HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
 class UnexpectedResponse(Exception): pass
 
 
+def unit_vector(vector):
+    """ Returns the unit vector of the vector.  """
+    return vector / np.linalg.norm(vector)
+
+
+def angle_between(v1, v2):
+    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+
+            >>> angle_between((1, 0, 0), (0, 1, 0))
+            1.5707963267948966
+            >>> angle_between((1, 0, 0), (1, 0, 0))
+            0.0
+            >>> angle_between((1, 0, 0), (-1, 0, 0))
+            3.141592653589793
+    """
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
 def get_angle(p1, p2):
-    return math.atan2(p1.y - p2.y, p1.x - p2.x)
+    return math.atan2(p1.x - p2.x, p1.y - p2.y)
 
 
 def get_distance(p1, p2):
-    return math.hypot(p1.x - p2.x, p1.y - p2.y)
+    return math.hypot(p1.y - p2.y, p1.x - p2.x)
+
+
+def test_angle():
+    print(math.degrees(get_angle(Position(0, 0, 0), Position(0, 1, 0))))
+    print(math.degrees(get_angle(Position(1, 0, 0), Position(0, 1, 0))))
+    print(math.degrees(get_angle(Position(0, 1, 0), Position(0, 1, 0))))
+    print(math.degrees(get_angle(Position(0, 0, 0), Position(1, 0, 0))))
+    print(math.degrees(get_angle(Position(0, 1, 0), Position(1, 0, 0))))
+    print(math.degrees(get_angle(Position(1, 0, 0), Position(1, 0, 0))))
+
+
+def get_angular_speed_from_angle(degree_angle):
+    return (1 / 180) * degree_angle
+
+
+def heading_to_point(robot_heading):
+    return Position(robot_heading.x, robot_heading.y, robot_heading.z)
+
+
+def choose_new_point_from_lookahead(target_point, robot_point, point_list, lookahead):
+    point_candidate = point_list[0]
+    for point in point_list:
+        if(point.timestamp > target_point.timestamp):
+            if(get_distance(robot_point, point) < lookahead and get_distance(robot_point, point) > get_distance(point_candidate, robot_point)):
+                
+
 
 
 if __name__ == '__main__':
+    test_angle()
     point_list = myJsonParser.read_json_file_to_list("Path-around-table-and-back.json")
 
-    for i in range(10):
-        first_point = point_list[i]
-        print("x: {0} y: {1}".format(first_point.position.x, first_point.position.y))
-        print("timestamp {0}".format(first_point.timestamp))
-        robot_position = get_robot_position()
-        distance = get_distance(robot_position, first_point.position)
-        angle_to_first_point = get_angle(robot_position, first_point.position)
-        print("distance {0}".format(distance))
-        print(math.degrees(angle_to_first_point))
+    target_point = point_list[0]
+    while True:
+        # robot_position = get_robot_position()
+        target_point = choose_new_point_from_lookahead(target_point, point_list)
+        robot_heading = getHeading()
+        robot_pos = get_robot_position()
+        print("distance: {}".format(get_distance(target_point.position, robot_pos)))
 
-    print('Sending commands to MRDS server', MRDS_URL)
-    # try:
-    #     print('Telling the robot to go straight ahead.')
-    #     pos = get_robot_position()
-    # except UnexpectedResponse as ex:
-    #     print('Unexpected response from server when sending speed commands:', ex)
-    #
-    # try:
-    #     laser = getLaser()
-    #     laserAngles = getLaserAngles()
-    #     print('The rightmost laser beam has angle %.3f deg from x-axis (streight forward) and distance %.3f meters.' % (
-    #         laserAngles[0], laser['Echoes'][0]
-    #     ))
-    #     print('Beam 1: %.3f Beam 269: %.3f Beam 270: %.3f' % (
-    #         laserAngles[0] * 180 / pi, laserAngles[269] * 180 / pi, laserAngles[270] * 180 / pi))
-    # except UnexpectedResponse as ex:
-    #     print('Unexpected response from server when reading laser data:', ex)
-    #
-    # try:
-    #     pose = get_robot_position()
-    #     print('Current position: ', pose['Pose']['Position'])
-    #     for t in range(30):
-    #         print('Current heading vector: X:{X:.3}, Y:{Y:.3}'.format(**getHeading()))
-    #         laser = getLaser()
-    #         print('Distance %.3f meters.' % (laser['Echoes'][135]))
-    #         if (laser['Echoes'][135] < 0.3):
-    #             print('Danger! Brace for impact! Hit the brakes!')
-    #             response = postSpeed(0, -0.1)
-    #         time.sleep(1)
-    # except UnexpectedResponse as ex:
-    #     print('Unexpected response from server when reading position:', ex)
+        cross = np.cross([robot_heading['X'], robot_heading['Y']], [target_point.position.x, target_point.position.y])
+        print("cross: {}".format(cross))
+
+        robot_vector = [robot_heading['X'], robot_heading['Y'], 0.1]
+        point_vector = [target_point.position.x, target_point.position.y, 0.1]
+        angle_to_first_point = angle_between(robot_vector, point_vector)
+        degree_angle = math.degrees(angle_to_first_point)
+        sfadfa = getHeading()
+        # print('Current heading vector: X:{X:.3}, Y:{Y:.3}'.format(**getHeading()))
+        if cross < 0:
+            postSpeed(-get_angular_speed_from_angle(degree_angle), 0.1)
+        else:
+            postSpeed(get_angular_speed_from_angle(degree_angle), 0.1)
+        time.sleep(0.2)
+
+        # try:
+        #     print('Telling the robot to go straight ahead.')
+        #     pos = get_robot_position()
+        # except UnexpectedResponse as ex:
+        #     print('Unexpected response from server when sending speed commands:', ex)
+        #
+        # try:
+        #     laser = getLaser()
+        #     laserAngles = getLaserAngles()
+        #     print('The rightmost laser beam has angle %.3f deg from x-axis (streight forward) and distance %.3f meters.' % (
+        #         laserAngles[0], laser['Echoes'][0]
+        #     ))
+        #     print('Beam 1: %.3f Beam 269: %.3f Beam 270: %.3f' % (
+        #         laserAngles[0] * 180 / pi, laserAngles[269] * 180 / pi, laserAngles[270] * 180 / pi))
+        # except UnexpectedResponse as ex:
+        #     print('Unexpected response from server when reading laser data:', ex)
+        #
+        # try:
+        #     pose = get_robot_position()
+        #     print('Current position: ', pose['Pose']['Position'])
+        #     for t in range(30):
+        #         print('Current heading vector: X:{X:.3}, Y:{Y:.3}'.format(**getHeading()))
+        #         laser = getLaser()
+        #         print('Distance %.3f meters.' % (laser['Echoes'][135]))
+        #         if (laser['Echoes'][135] < 0.3):
+        #             print('Danger! Brace for impact! Hit the brakes!')
+        #             response = postSpeed(0, -0.1)
+        #         time.sleep(1)
+        # except UnexpectedResponse as ex:
+        #     print('Unexpected response from server when reading position:', ex)
--- lokarriaexample3.py
+++ lokarriaexample3.py
@@ -1,4 +1,4 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 """
 Example demonstrating how to communicate with Microsoft Robotic Developer
 Studio 4 via the Lokarria http interface. 
@@ -12,33 +12,30 @@ Updated by Filip Allberg and Daniel Harr 2017-08-30 (actually converted to Pytho
 
 MRDS_URL = 'localhost:50000'
 
-import http.client, json, time, myJsonParser
-from math import sin, cos, pi, atan2
+import http.client, json, time
+from math import sin,cos,pi,atan2
 
 HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
 
-
 class UnexpectedResponse(Exception): pass
 
-
-def postSpeed(angularSpeed, linearSpeed):
+def postSpeed(angularSpeed,linearSpeed):
     """Sends a speed command to the MRDS server"""
     mrds = http.client.HTTPConnection(MRDS_URL)
-    params = json.dumps({'TargetAngularSpeed': angularSpeed, 'TargetLinearSpeed': linearSpeed})
-    mrds.request('POST', '/lokarria/differentialdrive', params, HEADERS)
+    params = json.dumps({'TargetAngularSpeed':angularSpeed,'TargetLinearSpeed':linearSpeed})
+    mrds.request('POST','/lokarria/differentialdrive',params,HEADERS)
     response = mrds.getresponse()
     status = response.status
-    # response.close()
+    #response.close()
     if status == 204:
         return response
     else:
         raise UnexpectedResponse(response)
 
-
 def getLaser():
     """Requests the current laser scan from the MRDS server and parses it into a dict"""
     mrds = http.client.HTTPConnection(MRDS_URL)
-    mrds.request('GET', '/lokarria/laser/echoes')
+    mrds.request('GET','/lokarria/laser/echoes')
     response = mrds.getresponse()
     if (response.status == 200):
         laserData = response.read()
@@ -46,49 +43,31 @@ def getLaser():
         return json.loads(laserData.decode())
     else:
         return response
-
-
-def get_robot_position():
-    """Reads the current position and orientation from the MRDS"""
-    mrds = http.client.HTTPConnection(MRDS_URL)
-    mrds.request('GET', '/lokarria/localization')
-    response = mrds.getresponse()
-    if (response.status == 200):
-        poseData = response.read()
-        response.close()
-        array = json.loads(poseData.decode())
-        position = myJsonParser.get_position(array)
-        return myJsonParser.create_position(position)
-    else:
-        return UnexpectedResponse(response)
-
-
-
+    
 def getLaserAngles():
     """Requests the current laser properties from the MRDS server and parses it into a dict"""
     mrds = http.client.HTTPConnection(MRDS_URL)
-    mrds.request('GET', '/lokarria/laser/properties')
+    mrds.request('GET','/lokarria/laser/properties')
     response = mrds.getresponse()
     if (response.status == 200):
         laserData = response.read()
         response.close()
         properties = json.loads(laserData.decode())
-        beamCount = int((properties['EndAngle'] - properties['StartAngle']) / properties['AngleIncrement'])
-        a = properties['StartAngle']  # +properties['AngleIncrement']
+        beamCount = int((properties['EndAngle']-properties['StartAngle'])/properties['AngleIncrement'])
+        a = properties['StartAngle']#+properties['AngleIncrement']
         angles = []
         while a <= properties['EndAngle']:
             angles.append(a)
-            a += pi / 180  # properties['AngleIncrement']
-        # angles.append(properties['EndAngle']-properties['AngleIncrement']/2)
+            a+=pi/180 #properties['AngleIncrement']
+        #angles.append(properties['EndAngle']-properties['AngleIncrement']/2)
         return angles
     else:
         raise UnexpectedResponse(response)
 
-
 def getPose():
     """Reads the current position and orientation from the MRDS"""
     mrds = http.client.HTTPConnection(MRDS_URL)
-    mrds.request('GET', '/lokarria/localization')
+    mrds.request('GET','/lokarria/localization')
     response = mrds.getresponse()
     if (response.status == 200):
         poseData = response.read()
@@ -97,84 +76,76 @@ def getPose():
     else:
         return UnexpectedResponse(response)
 
-
 def heading(q):
-    return rotate(q, {'X': 1.0, 'Y': 0.0, "Z": 0.0})
+    return rotate(q,{'X':1.0,'Y':0.0,"Z":0.0})
 
-
-def rotate(q, v):
-    return vector(qmult(qmult(q, quaternion(v)), conjugate(q)))
-
+def rotate(q,v):
+    return vector(qmult(qmult(q,quaternion(v)),conjugate(q)))
 
 def quaternion(v):
-    q = v.copy()
-    q['W'] = 0.0;
+    q=v.copy()
+    q['W']=0.0;
     return q
-
 
 def vector(q):
-    v = {}
-    v["X"] = q["X"]
-    v["Y"] = q["Y"]
-    v["Z"] = q["Z"]
+    v={}
+    v["X"]=q["X"]
+    v["Y"]=q["Y"]
+    v["Z"]=q["Z"]
     return v
 
-
 def conjugate(q):
-    qc = q.copy()
-    qc["X"] = -q["X"]
-    qc["Y"] = -q["Y"]
-    qc["Z"] = -q["Z"]
+    qc=q.copy()
+    qc["X"]=-q["X"]
+    qc["Y"]=-q["Y"]
+    qc["Z"]=-q["Z"]
     return qc
 
-
-def qmult(q1, q2):
-    q = {}
-    q["W"] = q1["W"] * q2["W"] - q1["X"] * q2["X"] - q1["Y"] * q2["Y"] - q1["Z"] * q2["Z"]
-    q["X"] = q1["W"] * q2["X"] + q1["X"] * q2["W"] + q1["Y"] * q2["Z"] - q1["Z"] * q2["Y"]
-    q["Y"] = q1["W"] * q2["Y"] - q1["X"] * q2["Z"] + q1["Y"] * q2["W"] + q1["Z"] * q2["X"]
-    q["Z"] = q1["W"] * q2["Z"] + q1["X"] * q2["Y"] - q1["Y"] * q2["X"] + q1["Z"] * q2["W"]
+def qmult(q1,q2):
+    q={}
+    q["W"]=q1["W"]*q2["W"]-q1["X"]*q2["X"]-q1["Y"]*q2["Y"]-q1["Z"]*q2["Z"]
+    q["X"]=q1["W"]*q2["X"]+q1["X"]*q2["W"]+q1["Y"]*q2["Z"]-q1["Z"]*q2["Y"]
+    q["Y"]=q1["W"]*q2["Y"]-q1["X"]*q2["Z"]+q1["Y"]*q2["W"]+q1["Z"]*q2["X"]
+    q["Z"]=q1["W"]*q2["Z"]+q1["X"]*q2["Y"]-q1["Y"]*q2["X"]+q1["Z"]*q2["W"]
     return q
-
-
+    
 def getHeading():
     """Returns the XY Orientation as a heading unit vector"""
     return heading(getPose()['Pose']['Orientation'])
-
 
 if __name__ == '__main__':
     print('Sending commands to MRDS server', MRDS_URL)
     try:
         print('Telling the robot to go straight ahead.')
-        response = postSpeed(0, 0.5)
+        response = postSpeed(0,0.5) 
         print('Waiting for a while...')
         time.sleep(3)
         print('Telling the robot to go in a circle.')
-        response = postSpeed(0.9, 0.1)
+        response = postSpeed(0.9,0.1)        
     except UnexpectedResponse as ex:
         print('Unexpected response from server when sending speed commands:', ex)
 
     try:
         laser = getLaser()
         laserAngles = getLaserAngles()
-        print('The rightmost laser bean has angle %.3f deg from x-axis (streight forward) and distance %.3f meters.' % (
-            laserAngles[0], laser['Echoes'][0]
+        print('The rightmost laser bean has angle %.3f deg from x-axis (streight forward) and distance %.3f meters.'%(
+            laserAngles[0],laser['Echoes'][0]
         ))
-        print('Beam 1: %.3f Beam 269: %.3f Beam 270: %.3f' % (
-        laserAngles[0] * 180 / pi, laserAngles[269] * 180 / pi, laserAngles[270] * 180 / pi))
+        print('Beam 1: %.3f Beam 269: %.3f Beam 270: %.3f'%( laserAngles[0]*180/pi, laserAngles[269]*180/pi, laserAngles[270]*180/pi))
     except UnexpectedResponse as ex:
         print('Unexpected response from server when reading laser data:', ex)
+
 
     try:
         pose = getPose()
         print('Current position: ', pose['Pose']['Position'])
-        for t in range(30):
+        for t in range(30):    
             print('Current heading vector: X:{X:.3}, Y:{Y:.3}'.format(**getHeading()))
             laser = getLaser()
-            print('Distance %.3f meters.' % (laser['Echoes'][135]))
+            print('Distance %.3f meters.'%(laser['Echoes'][135]))
             if (laser['Echoes'][135] < 0.3):
                 print('Danger! Brace for impact! Hit the brakes!')
-                response = postSpeed(0, -0.1)
+                response = postSpeed(0,-0.1)
             time.sleep(1)
     except UnexpectedResponse as ex:
         print('Unexpected response from server when reading position:', ex)
