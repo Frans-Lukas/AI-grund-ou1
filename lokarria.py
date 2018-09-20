271cc92e837006e0c973133ebd87c38047186d22
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-20 20:11:48 +0200

    woooorks, refactoring and some optimization left

--- lokarria.py
+++ lokarria.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
 
 import myJsonParser as myJsonParser
-import math
+from math import sin, cos, radians, degrees, hypot, atan2
 from RestLokarria import *
 from Point import *
 import numpy as np
 
 MRDS_URL = 'localhost:50000'
-
 HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
 
 
 class UnexpectedResponse(Exception): pass
-
-
-def unit_vector(vector):
-    """ Returns the unit vector of the vector.  """
-    return 0.2
 
 
 def angle_between(v1, v2):
@@ -25,32 +19,17 @@ def angle_between(v1, v2):
     y1 = v1[1]
     y2 = v2[1]
 
-    dot = x1 * x2 + y1 * y2  # dot product
-    det = x1 * y2 - y1 * x2  # determinant
-    angle = atan2(det, dot)  # atan2(y, x) or atan2(sin, cos)
-    return angle
+    dot = x1 * x2 + y1 * y2
+    det = x1 * y2 - y1 * x2
+    return atan2(det, dot)
 
 
 def get_angle(p1, p2):
-    return math.atan2(p1.x - p2.x, p1.y - p2.y)
+    return atan2(p1.x - p2.x, p1.y - p2.y)
 
 
 def get_distance(p1, p2):
-    return math.hypot(p1.x - p2.x, p1.y - p2.y)
-
-
-def test_angle():
-    print("000 and 010")
-    print(math.degrees(angle_between([0, 0, 0], [0, 1, 0])))
-    print(math.degrees(angle_between([1, 0, 0], [0, 1, 0])))
-    print(math.degrees(angle_between([0, 1, 0], [0, 1, 0])))
-    print(math.degrees(angle_between([0, 0, 0], [1, 0, 0])))
-    print(math.degrees(angle_between([0, 1, 0], [1, 0, 0])))
-    print(math.degrees(angle_between([1, 0, 0], [1, 0, 0])))
-
-
-def get_angular_speed_from_angle(degree_angle):
-    return 0.2
+    return hypot(p1.x - p2.x, p1.y - p2.y)
 
 
 def heading_to_point(robot_heading):
@@ -62,31 +41,27 @@ def get_heading_position():
     return Position(heading['X'], heading['Y'], 0)
 
 
-def choose_new_point_from_lookahead(current_point, robot_position, point_list, lookahead):
-    point_candidate = current_point
-    for point in point_list:
-        if point.timestamp > current_point.timestamp:
-            d1 = get_distance(point.position, robot_position)
-            d2 = get_distance(point_candidate.position, robot_position)
-            if lookahead > d1 > d2:
-                point_candidate = point
-    return point_candidate
+def get_index_for_next_lookahead(i, robot_pos, plist, lookahead, length):
+    point_candidate = plist[i]
+    for x in range(i, length):
+        point = plist[x]
+        if point.timestamp > point_candidate.timestamp:
+            d1 = get_distance(point.position, robot_pos)
+            if lookahead < d1:
+                return x
+    return i
 
 
-# 1. Ta ut heading vector A
-# 2. Ta ut vector vi ska till B
-# 3. Normalisera vektorerna A och B ( Längden ska vara 1 )
-# 4. Ta reda på hur många grader man behöver vända vektor A för att den ska lägga sig på X-axel.
-# 5. Rotera även vektor B med lika många grader
-# 6. arcsin(By) ger om det är + lr -
-# 7. arcos(Bx) ger vinkel
+def testing_get_first_position(plist, length, lookahead):
+    first_position = plist[0]
+    for x in range(length):
+        second_position = plist[x]
+        if get_distance(first_position.position, second_position.position) > lookahead:
+            return x
+
 
 def points_to_vector(p1, p2):
     return [p2.x - p1.x, p2.y - p1.y]
-
-
-def vector_to_position(v):
-    return Position(v[0], v[1], 0)
 
 
 def position_to_vector(position):
@@ -97,30 +72,60 @@ def normalize_vector(v):
     return [x / np.linalg.norm(v) for x in v]
 
 
-def rad_to_rotation_matrix(rad):
-    c, s = np.cos(rad), np.sin(rad)
-    return np.array(((c, -s), (s, c)))
+def rotate_point(point, a, center_point=(0, 0)):
+    angle_rad = radians(a % 360)
+    new_point = (point[0] - center_point[0], point[1] - center_point[1])
+    new_point = (new_point[0] * cos(angle_rad) - new_point[1] * sin(angle_rad),
+                 new_point[0] * sin(angle_rad) + new_point[1] * cos(angle_rad))
+    new_point = (new_point[0] + center_point[0], new_point[1] + center_point[1])
+    return new_point
+
+
+def get_angular_speed(degree_angle):
+    return 2 * (degree_angle / 100)
+
+
+def get_speed(degree_angle):
+    return 1 - (get_angular_speed(degree_angle)/2)
 
 
 if __name__ == '__main__':
-    test_angle()
-    point_list = myJsonParser.read_json_file_to_list("Path-to-bed.json")
-    point_list.sort(key=lambda x: x.timestamp, reverse=False)
+    point_list = myJsonParser.read_json_file_to_list("Path-around-table-and-back.json")
+    point_list.sort(key=lambda x: x.timestamp)
 
-    target_point = point_list[0]
-    lookahead = 0.5
+    list_length = len(point_list)
+    lookahead = 1
+    target_point = point_list[testing_get_first_position(point_list, list_length, lookahead)]
+    index = 0
     while True:
+
+        # 1. Ta ut heading vector A
         heading_vector = position_to_vector(get_heading_position())
 
+        # 2. Ta ut vector B som vi ska till
         robot_position = get_robot_position()
-        target_point = choose_new_point_from_lookahead(target_point, robot_position, point_list, lookahead)
-
+        print(get_distance(target_point.position, robot_position))
+        if get_distance(target_point.position, robot_position) < 0.9:
+            # target_point = choose_new_point_from_lookahead(target_point, robot_position, point_list, lookahead)
+            index = get_index_for_next_lookahead(index, robot_position, point_list, lookahead, list_length)
+            target_point = point_list[index]
         to_point_vector = points_to_vector(robot_position, target_point.position)
 
-        angle = math.degrees(angle_between(heading_vector, to_point_vector))
+        # 3. Normalisera vektorerna A och B ( Längden ska vara 1 )
+        heading_vector = normalize_vector(heading_vector)
+        to_point_vector = normalize_vector(to_point_vector)
 
-        if angle > 0:
-            postSpeed(0.4, 0.1)
+        # 4. Ta reda på hur många grader man behöver vända vektor A för att den ska lägga sig på X-axel.
+        angle = degrees(angle_between(heading_vector, [1, 0]))
+
+        # 5. Rotera vektor B med lika många grader
+        to_point_vector = rotate_point(to_point_vector, angle, center_point=(0, 0))
+
+        # 6. arcsin(By) ger om det är + lr -
+        # 7. arcos(Bx) ger vinkel
+        angle = degrees(np.arccos(to_point_vector[0]))
+        if np.arcsin(to_point_vector[1]) > 0:
+            postSpeed(get_angular_speed(angle), get_speed(angle))
         else:
-            postSpeed(-0.4, 0.1)
-        time.sleep(0.1)
+            postSpeed(-get_angular_speed(angle), get_speed(angle))
+        time.sleep(0.01)
