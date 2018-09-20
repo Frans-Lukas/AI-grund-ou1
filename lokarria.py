40fce255c5f8946bbb8f4d69a966bd58c8e9db96
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-09-20 14:06:58 +0200

    i tried, but failed ):

--- lokarria.py
+++ lokarria.py
@@ -16,22 +16,19 @@ class UnexpectedResponse(Exception): pass
 
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
-    return vector / np.linalg.norm(vector)
+    return 0.2
 
 
 def angle_between(v1, v2):
-    """ Returns the angle in radians between vectors 'v1' and 'v2'::
+    x1 = v1[0]
+    x2 = v2[0]
+    y1 = v1[1]
+    y2 = v2[1]
 
-            >>> angle_between((1, 0, 0), (0, 1, 0))
-            1.5707963267948966
-            >>> angle_between((1, 0, 0), (1, 0, 0))
-            0.0
-            >>> angle_between((1, 0, 0), (-1, 0, 0))
-            3.141592653589793
-    """
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+    dot = x1 * x2 + y1 * y2  # dot product
+    det = x1 * y2 - y1 * x2  # determinant
+    angle = atan2(det, dot)  # atan2(y, x) or atan2(sin, cos)
+    return angle
 
 
 def get_angle(p1, p2):
@@ -43,16 +40,17 @@ def get_distance(p1, p2):
 
 
 def test_angle():
-    print(math.degrees(get_angle(Position(0, 0, 0), Position(0, 1, 0))))
-    print(math.degrees(get_angle(Position(1, 0, 0), Position(0, 1, 0))))
-    print(math.degrees(get_angle(Position(0, 1, 0), Position(0, 1, 0))))
-    print(math.degrees(get_angle(Position(0, 0, 0), Position(1, 0, 0))))
-    print(math.degrees(get_angle(Position(0, 1, 0), Position(1, 0, 0))))
-    print(math.degrees(get_angle(Position(1, 0, 0), Position(1, 0, 0))))
+    print("000 and 010")
+    print(math.degrees(angle_between([0, 0, 0], [0, 1, 0])))
+    print(math.degrees(angle_between([1, 0, 0], [0, 1, 0])))
+    print(math.degrees(angle_between([0, 1, 0], [0, 1, 0])))
+    print(math.degrees(angle_between([0, 0, 0], [1, 0, 0])))
+    print(math.degrees(angle_between([0, 1, 0], [1, 0, 0])))
+    print(math.degrees(angle_between([1, 0, 0], [1, 0, 0])))
 
 
 def get_angular_speed_from_angle(degree_angle):
-    return degree_angle / 180
+    return 0.2
 
 
 def heading_to_point(robot_heading):
@@ -68,9 +66,9 @@ def choose_new_point_from_lookahead(current_point, robot_position, point_list, l
     point_candidate = current_point
     for point in point_list:
         if point.timestamp > current_point.timestamp:
-            dist_between_robot_and_new_point = get_distance(robot_position, point.position)
-            dist_between_robot_and_candidate = get_distance(robot_position, point_candidate.position)
-            if dist_between_robot_and_new_point > lookahead and dist_between_robot_and_new_point > dist_between_robot_and_candidate:
+            d1 = get_distance(point.position, robot_position)
+            d2 = get_distance(point_candidate.position, robot_position)
+            if lookahead > d1 > d2:
                 point_candidate = point
     return point_candidate
 
@@ -83,12 +81,16 @@ def choose_new_point_from_lookahead(current_point, robot_position, point_list, l
 # 6. arcsin(By) ger om det är + lr -
 # 7. arcos(Bx) ger vinkel
 
-def position_to_vector(position):
-    return [position.x, position.y]
+def points_to_vector(p1, p2):
+    return [p2.x - p1.x, p2.y - p1.y]
 
 
 def vector_to_position(v):
     return Position(v[0], v[1], 0)
+
+
+def position_to_vector(position):
+    return [position.x, position.y]
 
 
 def normalize_vector(v):
@@ -97,34 +99,44 @@ def normalize_vector(v):
 
 def rad_to_rotation_matrix(rad):
     c, s = np.cos(rad), np.sin(rad)
-    R = np.array(((c, -s), (s, c)))
-    return R
+    return np.array(((c, -s), (s, c)))
 
 
 if __name__ == '__main__':
     test_angle()
-    point_list = myJsonParser.read_json_file_to_list("Path-around-table-and-back.json")
+    point_list = myJsonParser.read_json_file_to_list("Path-to-bed.json")
+    point_list.sort(key=lambda x: x.timestamp, reverse=False)
 
-    target_point = point_list[len(point_list) - 1]
-    target_vector = target_point.position
-    lookahead = 0.1
+    target_point = point_list[0]
+    lookahead = 1
     while True:
         heading_vector = position_to_vector(get_heading_position())
+
         robot_position = get_robot_position()
-        #target_point = choose_new_point_from_lookahead(target_point, robot_position, point_list, lookahead  )
-        print("x: {} y: {}".format(target_point.position.x, target_point.position.y))
-        target_vector = position_to_vector(target_point.position)
+
+        target_point = choose_new_point_from_lookahead(target_point, robot_position, point_list, lookahead)
+
+        target_vector = points_to_vector(target_point.position, robot_position)
 
         target_normalized_vector = normalize_vector(target_vector)
-        robot_normalized_vector = normalize_vector(heading_vector)
-        angle_between_heading_and_x = get_angle(vector_to_position(robot_normalized_vector),
-                                                vector_to_position([1, 0, 0]))
+
+        angle_between_heading_and_x = angle_between([1, 0, 0], [heading_vector[0], heading_vector[1], 0])
+
+        angle_between_heading_and_x = math.radians(math.degrees(angle_between_heading_and_x) + 90)
+
         rotation_matrix = rad_to_rotation_matrix(angle_between_heading_and_x)
+
         target_normalized_and_rotated_vector = np.matmul(rotation_matrix, target_normalized_vector)
+
         left_or_right = np.arcsin(target_normalized_and_rotated_vector[1])
+
         rotation_angle = np.arccos(target_normalized_and_rotated_vector[0])
-        if left_or_right < 0:
-            postSpeed(get_angular_speed_from_angle(math.degrees(rotation_angle)), 0)
+
+        print(math.degrees(rotation_angle))
+
+        speed = 0.2
+        if left_or_right > 0:
+            postSpeed(get_angular_speed_from_angle(math.degrees(rotation_angle)), speed)
         else:
-            postSpeed(-get_angular_speed_from_angle(math.degrees(rotation_angle)), 0)
+            postSpeed(-get_angular_speed_from_angle(math.degrees(rotation_angle)), speed)
         time.sleep(0.1)
