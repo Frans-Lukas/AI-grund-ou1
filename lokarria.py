fa5214df3af4912da8b1aefc26aec3c21d1e7d4d
Frans-lukas Lövenvald <c16fld@cs.umu.se>
2018-09-20 14:26:04 +0200

    finally WORKSgit add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .

--- lokarria.py
+++ lokarria.py
@@ -108,35 +108,19 @@ if __name__ == '__main__':
     point_list.sort(key=lambda x: x.timestamp, reverse=False)
 
     target_point = point_list[0]
-    lookahead = 1
+    lookahead = 0.5
     while True:
         heading_vector = position_to_vector(get_heading_position())
 
         robot_position = get_robot_position()
-
         target_point = choose_new_point_from_lookahead(target_point, robot_position, point_list, lookahead)
 
-        target_vector = points_to_vector(target_point.position, robot_position)
+        to_point_vector = points_to_vector(robot_position, target_point.position)
 
-        target_normalized_vector = normalize_vector(target_vector)
+        angle = math.degrees(angle_between(heading_vector, to_point_vector))
 
-        angle_between_heading_and_x = angle_between([1, 0, 0], [heading_vector[0], heading_vector[1], 0])
-
-        angle_between_heading_and_x = math.radians(math.degrees(angle_between_heading_and_x) + 90)
-
-        rotation_matrix = rad_to_rotation_matrix(angle_between_heading_and_x)
-
-        target_normalized_and_rotated_vector = np.matmul(rotation_matrix, target_normalized_vector)
-
-        left_or_right = np.arcsin(target_normalized_and_rotated_vector[1])
-
-        rotation_angle = np.arccos(target_normalized_and_rotated_vector[0])
-
-        print(math.degrees(rotation_angle))
-
-        speed = 0.2
-        if left_or_right > 0:
-            postSpeed(get_angular_speed_from_angle(math.degrees(rotation_angle)), speed)
+        if angle > 0:
+            postSpeed(0.4, 0.1)
         else:
-            postSpeed(-get_angular_speed_from_angle(math.degrees(rotation_angle)), speed)
+            postSpeed(-0.4, 0.1)
         time.sleep(0.1)
