13d504a7ccb94c6a4bba35870cd860c764319531
Jakub Jagiello <kubajagiello94@gmail.com>
2018-09-25 10:45:10 +0200

    done

--- lokarria.py
+++ lokarria.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python3
 
-import myJsonParser as myJsonParser
-from math import sin, cos, radians, degrees, hypot, atan2
+from math import radians, degrees, hypot
 from RestLokarria import *
 from Point import *
 import numpy as np
 import time
 
 
-MRDS_URL = 'localhost:50000'
-HEADERS = {"Content-type": "application/json", "Accept": "text/json"}
-
-
-class UnexpectedResponse(Exception): pass
-
-
 def angle_between(v1, v2):
+    """
+    :param v1: vector
+    :param v2: vector
+    :return: angle between given vectors
+    """
     x1 = v1[0]
     x2 = v2[0]
     y1 = v1[1]
@@ -26,6 +23,11 @@ def angle_between(v1, v2):
 
 
 def get_distance(p1, p2):
+    """
+    :param p1: point with x and y coordinates
+    :param p2: point with x and y coordinates
+    :return: distance between p1 and p2
+    """
     return hypot(p1.x - p2.x, p1.y - p2.y)
 
 
@@ -34,22 +36,37 @@ def get_heading_position():
     return Position(h['X'], h['Y'], 0)
 
 
-def get_index_for_next_lookahead(i, robot_pos, plist, lookahead, length):
+def get_index_for_next_lookahead(i, robot_pos, plist, lookahead_distance, length):
+    """
+    Finds next lookahead position for robot
+    :param i: index
+    :param robot_pos: current position for robot
+    :param plist: list of points
+    :param lookahead_distance: min distance
+    :param length: length of list
+    :return: index for point in list
+    """
     point_candidate = plist[i]
     for x in range(i, length):
         point = plist[x]
         if point.timestamp > point_candidate.timestamp:
             d1 = get_distance(point.position, robot_pos)
-            if lookahead < d1:
+            if lookahead_distance < d1:
                 return x
     return i
 
 
-def testing_get_first_position(plist, length, lookahead):
+def get_first_position(plist, length, lookahead_distance):
+    """
+    :param plist: list of points
+    :param length: length of list
+    :param lookahead_distance: min distance
+    :return: first point for robot
+    """
     first_position = plist[0]
     for x in range(length):
         second_position = plist[x]
-        if get_distance(first_position.position, second_position.position) > lookahead:
+        if get_distance(first_position.position, second_position.position) > lookahead_distance:
             return x
 
 
@@ -62,11 +79,21 @@ def position_to_vector(position):
 
 
 def normalize_vector(v):
+    """
+    :param v: any vector
+    :return: vector with magnitude 1
+    """
     return [x / np.linalg.norm(v) for x in v]
 
 
-def rotate_point(point, a, center_point=(0, 0)):
-    angle_rad = radians(a % 360)
+def rotate_point(point, angle_to_rotate, center_point=(0, 0)):
+    """
+    :param point: Point with x and y coordinates
+    :param angle_to_rotate: Angle that given point will be rotated with
+    :param center_point: Point that will be the base for rotation
+    :return: rotated point
+    """
+    angle_rad = radians(angle_to_rotate % 360)
     new_point = (point[0] - center_point[0], point[1] - center_point[1])
     new_point = (new_point[0] * cos(angle_rad) - new_point[1] * sin(angle_rad),
                  new_point[0] * sin(angle_rad) + new_point[1] * cos(angle_rad))
@@ -81,13 +108,7 @@ def get_angular_speed(degree_angle):
 def get_speed(degree_angle):
     return 1
 
-'''
-BEST SPECS HITTILS: 
-lookahead : 1
-min_distance < 0.95
-angular_speed : return 2 * (degree_angle / 30)
-speed : 1.65 - (get_angular_speed(degree_angle)/2)
-'''
+
 if __name__ == '__main__':
     print('Waiting for robot to start the race...')
     point_list = myJsonParser.read_json_file_to_list("Path-around-table-and-back.json")
@@ -95,7 +116,7 @@ if __name__ == '__main__':
 
     list_length = len(point_list)
     lookahead = 1.05
-    target_point = point_list[testing_get_first_position(point_list, list_length, lookahead)]
+    target_point = point_list[get_first_position(point_list, list_length, lookahead)]
     index = 0
     start = time.time()
     print("Go")
@@ -113,14 +134,16 @@ if __name__ == '__main__':
                 print("Time: " + str((end - start)))
                 exit()
 
-        to_point_vector = points_to_vector(robot_position, target_point.position)
-        heading_vector = normalize_vector(heading_vector)
-        to_point_vector = normalize_vector(to_point_vector)
-        angle = degrees(angle_between(heading_vector, [1, 0]))
-        to_point_vector = rotate_point(to_point_vector, angle, center_point=(0, 0))
-        angle = degrees(np.arccos(to_point_vector[0]))
+        lookahead_vector = points_to_vector(robot_position, target_point.position)
 
-        if np.arcsin(to_point_vector[1]) > 0:
+        heading_vector = normalize_vector(heading_vector)
+        lookahead_vector = normalize_vector(lookahead_vector)
+
+        angle = degrees(angle_between(heading_vector, [1, 0]))
+        lookahead_vector = rotate_point(lookahead_vector, angle, center_point=(0, 0))
+        angle = degrees(np.arccos(lookahead_vector[0]))
+
+        if np.arcsin(lookahead_vector[1]) > 0:
             postSpeed(get_angular_speed(angle), get_speed(angle))
         else:
             postSpeed(-get_angular_speed(angle), get_speed(angle))
